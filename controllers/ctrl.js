2a675431cd67bed269710c61d4094bb0d99712fb
Abraham <abraham.kone@fyle-group.co>
2025-03-17 18:09:51 +0000

    Implémentation des contrôleurs de lecture (READ)

--- controllers/ctrl.js
+++ controllers/ctrl.js
@@ -1,8 +1,22 @@
 import Product from "../models/products.js";
 import producValidation from "../validation/productValidation.js";
 
-const getAll = (req, res) => {};
-const getOne = (req, res) => {};
+const getAll = (req, res) => {
+  Product.findAll({ attributes: { exclude: ["createdAt", "updatedAt"] } })
+    .then((products) => res.status(200).json(products))
+    .catch((err) => res.status(500).json(err));
+};
+
+const getOne = (req, res) => {
+  const { id } = req.params;
+  Product.findByPk(id)
+    .then((product) => {
+      if (!product) return res.status(404).json({ msg: "Not found" });
+      res.status(200).json(product);
+    })
+    .catch((err) => res.status(500).json(err));
+};
+
 const createOne = (req, res) => {
   const { body } = req;
   const { error } = producValidation(body);
@@ -12,6 +26,7 @@ const createOne = (req, res) => {
     .then(res.status(201).json({ msg: "Created ressources" }))
     .catch((err) => res.status(500).json(err));
 };
+
 const updateOne = (req, res) => {};
 const deleteOne = (req, res) => {};
 
